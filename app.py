4e58fce1e7d56005d301e4bf15c53bac5c3f685a
Sally Poulsen <sallypoulsen@Sallys-MacBook-Pro.local>
2024-06-29 15:30:42 -0600

    Adds in routes

--- app.py
+++ app.py
@@ -16,5 +16,19 @@ def index():
 def new_projects():
     return 'new projects'
 
+
+@app.route('/projects/<id>')
+def project_by_id(id):
+    return id
+
+
+@app.route('/projects/<id>/edit')
+def edit_project_by_id(id):
+    return id
+
+@app.route('/projects/<id>/delete')
+def delete_project_by_id(id):
+    return id
+
 if __name__ =='__main__':
     app.run(debug=True, port=8000, host='127.0.0.1')
