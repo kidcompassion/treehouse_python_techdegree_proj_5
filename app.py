f5aab65a7891bd436999c1aeda98fd4c534de824
Sally Poulsen <sallypoulsen@Sallys-MacBook-Pro.local>
2024-07-06 20:36:16 -0600

    Adds template for editing posts. Adds jinja logic to load dynamic data into detail.html. Adds logic to format date and list for the page.

--- app.py
+++ app.py
@@ -8,11 +8,15 @@ def index():
 
 
 @app.route('/project/new', methods=['GET', 'POST'])
-def add_new_project():
+def add_project():
     if request.form:
+
+        # Format incoming date string as datetime object
+        split_date = request.form['date'].split("-")
+
         new_project = Project(
             title = request.form['title'],
-            date = datetime.datetime.now(),#request.form['date'],
+            date = datetime.date(year=int(split_date[0]), month=int(split_date[1]), day = 1),
             skills = request.form['skills'],
             description = request.form['desc'],
             url = request.form['url']
@@ -20,24 +24,54 @@ def add_new_project():
     
         db.session.add(new_project)
         db.session.commit()
+        print(request.form)
         return redirect(url_for('index'))
     return render_template('projectform.html')
 
 
+
+
+
 @app.route('/project/<id>')
-def project_by_id(id):
-    print(id)
+def project(id):
     project = Project.query.get_or_404(id)
+    # format date for the details page
+    project.date = project.date.strftime("%b %Y")
+
+    # format skills for a bulleted list
+    project.skills = project.skills.split(",")
+    
+
     return render_template('detail.html', project = project)
 
 
-@app.route('/project/<id>/edit')
-def edit_project_by_id(id):
-    return id
+@app.route('/project/<id>/edit', methods = ['GET', 'POST'])
+def edit_project(id):
+    project = Project.query.get(id)
+    if request.form:
+        project.title = request.form['title']
+        project.date = request.form['date']
+        project.skills = request.form['skills']
+        project.description = request.form['description']
+        project.url = request.form['url']
+
+        db.session.add(project)
+        db.session.commit()
+        return redirect(url_for('index'))
+    
+    return render_template('editproject.html', project=project)
+    
+
+
 
 @app.route('/project/<id>/delete')
-def delete_project_by_id(id):
-    return id
+def delete_project(id):
+    project = Project.query.get(id)
+    db.session.delete(project)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
 
 if __name__ =='__main__':
     with app.app_context():
