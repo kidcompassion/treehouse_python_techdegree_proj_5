8232858566851c45a1982dc5131b9275b1e750a7
Sally Poulsen <sallypoulsen@Sallys-MacBook-Pro.local>
2024-07-07 15:05:15 -0600

    Adds READ ME and removes unused template

---
+++ README.md
@@ -0,0 +1,12 @@
+# Portfolio with SQLAlchemy
+
+This is a simple portfolio website built in Flask, including a form to allow updating of new projects.
+
+To run:
+* Clone repo
+* From root, run python3 app.py
+
+Things learned:
+* Basic Flask usage, including routes and templating
+* Passing variables from route to template
+* Constructing URLs in Flask
--- app.py
+++ app.py
@@ -10,13 +10,14 @@ def index():
 
 @app.route('/project/new', methods=['GET', 'POST'])
 def add_project():
+
+    #Query all projects so that the drop down menu will work on interior pages
+    projects = Project.query.all()
     if request.form:
 
         # Format incoming date string as datetime object
         date_split = request.form['date'].split("-")
         
-        
-
         new_project = Project(
             title = request.form['title'],
             date = datetime.date(year=int(date_split[0]), month=int(date_split[1]), day = 1),
@@ -29,14 +30,13 @@ def add_project():
         db.session.commit()
         print(request.form)
         return redirect(url_for('index'))
-    return render_template('projectform.html')
-
-
-
+    return render_template('projectform.html', projects=projects)
 
 
 @app.route('/project/<id>')
 def project(id):
+    # Query for all projects so the dropdown nav will work on this route
+    projects = Project.query.all()
     project = Project.query.get_or_404(id)
     # format date for the details page
     project.date = project.date.strftime("%b %Y")
@@ -44,38 +44,30 @@ def project(id):
     # format skills for a bulleted list
     project.skills = project.skills.split(",")
     
-
-    return render_template('detail.html', project = project)
-
-
-
+    return render_template('detail.html', project = project, projects=projects)
 
 
 @app.route('/project/<id>/edit', methods = ['GET', 'POST'])
 def edit_project(id):
-    print("fires")
+    projects = Project.query.all()
     project = Project.query.get(id)
+    # incoming date from DB is an object.
     starting_project_date = project.date
+    # Convert incoming date to correct format for value of datepicker
     project.date = project.date.strftime("%Y-%m")
-    
-    # if project.date has changed
-    # convert it to a datetime obj
-    
 
+    #on Form submit
     if request.form:
-        print("fires inside request")
         project.title = request.form['title']
-        
+        # If date picker value has not changed, just pass back the date obj we started with
         if project.date == request.form['date']:
             project.date = starting_project_date
+        
         else:
-            print("fires in else")
+            # If date picker has changed, split the value from the date picker
             split_date = request.form['date'].split("-")
-            
+            # Pass split value into date obj and store it in database
             project.date = datetime.date(year=int(split_date[0]), month=int(split_date[1]), day = 1)
-            
-        
-        
         project.skills = request.form['skills']
         project.description = request.form['desc']
         project.url = request.form['url']
@@ -84,10 +76,8 @@ def edit_project(id):
         db.session.commit()
         return redirect(url_for('index'))
     
-    return render_template('editproject.html', project=project)
+    return render_template('editproject.html', project=project, projects=projects)
     
-
-
 
 @app.route('/project/<id>/delete')
 def delete_project(id):
@@ -95,7 +85,6 @@ def delete_project(id):
     db.session.delete(project)
     db.session.commit()
     return redirect(url_for('index'))
-
 
 
 if __name__ =='__main__':
